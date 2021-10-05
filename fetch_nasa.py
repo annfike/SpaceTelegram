036865440305113e076095ca8a4a7265c4576af5
Anna <44573387+annfike@users.noreply.github.com>
2021-10-05 15:17:37 +0300

    Update fetch_nasa.py

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -1,23 +1,29 @@
-import datetime
 import requests
 from fetch_spacex import load_image
 
 
-def get_epic_pics(token_nasa, url):
-    date = datetime.datetime(year=2021, month=9, day=21)
-    # объясняю строчку с датой - я не могу вытащить фотки от 'сегодня',
-    # последние фотки там почему-то от 21.09
-    # https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY
-    payload = {'date': date.date(), 'api_key': token_nasa}
-    response = requests.get(url, params=payload)
+def get_apod_pics(path, token):
+  url = 'https://api.nasa.gov/planetary/apod'
+  payload = {'count': '10', 'api_key': token}
+  response = requests.get(url, params=payload)
+  response.raise_for_status()
+  response = response.json()
+  for index, pic in enumerate(response, start=1):
+    filename = f'nasa{index}.jpg'
+    load_image(path, filename, pic['url'])
+
+
+def get_epic_pics(path, token_nasa, date):
+    payload = {'api_key': token_nasa}
+    response = requests.get(f"https://api.nasa.gov/EPIC/api/natural/date/{date}", params=payload)
     response.raise_for_status()
     response = response.json()
-    for i, pic in enumerate(response, start=1):
+    year, month, day = date.split("-")
+    for index, pic in enumerate(response, start=1):
         image = pic['image']
-        formatted_date = date.strftime('%Y/%m/%d')
-        filename = f'earth{i}.jpg'
-        link = f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{image}.png?api_key={token_nasa}'
-        load_image('files', filename, link)
+        filename = f'earth{index}.jpg'
+        link = requests.get(f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{image}.png', params=payload)
+        load_image(path, filename, link)
 
 
 
