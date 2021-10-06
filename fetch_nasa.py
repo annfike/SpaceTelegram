90e669a526c77a5bad57932a825823ccd2e81aa6
Anna <44573387+annfike@users.noreply.github.com>
2021-10-06 13:18:40 +0300

    отформатировала код

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -3,19 +3,20 @@ from load_image import load_image
 
 
 def get_apod_pics(path, token):
-  url = 'https://api.nasa.gov/planetary/apod'
-  payload = {'count': '10', 'api_key': token}
-  response = requests.get(url, params=payload)
-  response.raise_for_status()
-  response = response.json()
-  for index, pic in enumerate(response, start=1):
-    filename = f'nasa{index}.jpg'
-    load_image(path, filename, pic['url'])
+    url = 'https://api.nasa.gov/planetary/apod'
+    payload = {'count': '10', 'api_key': token}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    response = response.json()
+    for index, pic in enumerate(response, start=1):
+        filename = f'nasa{index}.jpg'
+        load_image(path, filename, pic['url'])
 
 
 def get_epic_pics(path, token_nasa, date):
     payload = {'api_key': token_nasa}
-    response = requests.get(f"https://api.nasa.gov/EPIC/api/natural/date/{date}", params=payload)
+    response = requests.get(f"https://api.nasa.gov/EPIC/api/natural/date/{date}",
+               params=payload)
     response.raise_for_status()
     response = response.json()
     year, month, day = date.split("-")
