53cbb64896e97fac6cd886b6c96e7ddd56b50d83
Anna <44573387+annfike@users.noreply.github.com>
2021-10-06 13:15:24 +0300

    Update fetch_spacex.py
    изменила название переменной response

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -7,7 +7,7 @@ def fetch_spacex_launch(path, launch):
     response = requests.get(url)
     response.raise_for_status()
     response = response.json()
-    response = response['links']['flickr_images']
-    for index, link in enumerate(response, start=1):
+    images = response['links']['flickr_images']
+    for index, link in enumerate(images, start=1):
         filename = f'spacex{index}.jpg'
         load_image(path, filename, link)
