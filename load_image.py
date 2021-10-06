57b7bdbacc16287e9b1ddc57e7a0c9ceb3dca490
Anna <44573387+annfike@users.noreply.github.com>
2021-10-06 12:10:52 +0300

    Create load_image.py
    создан файл для функции load_image

---
+++ load_image.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import requests
+
+
+def load_image(path, filename, url, params = None):
+    path = Path(path)
+    filepath = path / filename
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with filepath.open('wb') as file:
+        file.write(response.content)
