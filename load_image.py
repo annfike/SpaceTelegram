63480e04902e13740f775e38e2c4d6ea5d32a90f
Anna <44573387+annfike@users.noreply.github.com>
2021-10-07 10:37:02 +0300

    исправила Path, get_extension

--- load_image.py
+++ load_image.py
@@ -2,9 +2,8 @@ from pathlib import Path
 import requests
 
 
-def load_image(path, filename, url, params = None):
-    path = Path(path)
-    filepath = path / filename
+def load_image(path, filename, url, params=None):
+    filepath = Path(path) / filename
     response = requests.get(url, params=params)
     response.raise_for_status()
     with filepath.open('wb') as file:
