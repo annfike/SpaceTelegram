448b82b27af1a5eba195ed3dc82550e4d048d913
Anna <44573387+annfike@users.noreply.github.com>
2021-10-07 11:12:49 +0300

    добавила функцию отправки фото в Телеграм

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 import os
+import pathlib
 from os import listdir
 from os.path import isfile
 from os.path import join as joinpath
 from time import sleep
-import pathlib
 
 import telegram
 from dotenv import load_dotenv
 
-from fetch_nasa import get_epic_pics, get_apod_pics
+from fetch_nasa import fetch_apod_pics, fetch_epic_pics
 from fetch_spacex import fetch_spacex_launch
+
+
+def send_pics_tg(tg_token, tg_channel, path):
+    bot = telegram.Bot(token=tg_token)
+    bot.send_message(chat_id=tg_channel, text='Hi')
+    for files in listdir(path):
+        filename = joinpath(path, files)
+        if isfile(filename):
+            with open(filename, 'rb') as file:
+                bot.send_document(chat_id=tg_channel, document=file)
+        sleep(86400)
 
 
 def main():
     load_dotenv()
-    token_nasa = os.getenv('NASA_TOKEN')
-    token_tg = os.getenv('TG_BOT_TOKEN')
-    channel_tg = os.getenv('TG_CHANNEL')
-    bot = telegram.Bot(token=token_tg)
+    nasa_token = os.getenv('NASA_TOKEN')
+    tg_token = os.getenv('TG_BOT_TOKEN')
+    tg_channel = os.getenv('TG_CHANNEL')
     path = 'files'
     pathlib.Path(path).mkdir(exist_ok=True)
     fetch_spacex_launch(path, '67')
-    get_apod_pics(path, token_nasa)
-    get_epic_pics(path, token_nasa, '2021-10-03')
-
+    fetch_apod_pics(path, nasa_token)
+    fetch_epic_pics(path, nasa_token, '2021-10-03')
 
     while True:
-        bot.send_message(chat_id=channel_tg, text='Hi')
-        for files in listdir(path):
-            filename = joinpath(path, files)
-            if isfile(filename):
-                with open(filename, 'rb') as file:
-                    f = file.read()
-                bot.send_document(chat_id=channel_tg, document=f)
-            sleep(86400)
+        send_pics_tg(tg_token, tg_channel, path)
+
 
 
 if __name__ == '__main__':
